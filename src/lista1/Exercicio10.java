7f9631a2237f29ce020fb02d22104883289c8461
Amanda <amandacruzlima.ac@gmail.com>
2024-03-20 16:58:57 -0300

    ajustes

---
+++ src/lista1/Exercicio10.java
@@ -0,0 +1,15 @@
+package lista1;
+public class Exercicio10 {
+    public static void main(String[] args) {
+        int numerador = 1;
+        int denominador;
+        double soma = 0;
+
+        for (denominador = 1; denominador <= 50; denominador++) {
+            numerador = numerador + 2;
+            soma = soma + ((double) numerador /denominador);
+        }
+
+        System.out.printf("A soma e %.2f", soma);
+    }
+}
