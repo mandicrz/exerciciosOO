f57700437438a4198b0b467365e43dd3784eae20
Amanda Cruz <amandacruzlima.ac@gmail.com>
2024-03-20 20:24:40 -0300

    exercicios finalizados

---
+++ src/lista1/Exercicio11.java
@@ -0,0 +1,40 @@
+package lista1;
+import java.util.Scanner;
+
+public class Exercicio11 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num, posH = 0, posL = 0;
+        double highest = 0, lowest = 0;
+
+        System.out.println("Digite o numero de alunos: ");
+        num = scanner.nextInt();
+
+        double[] height = new double[num];
+
+        System.out.println("Digite as alturas: ");
+
+        for (int i = 0; i < num; i++){
+            height[i]= scanner.nextDouble();
+            if (i == 0){
+                highest = height[i];
+                lowest = height[i];
+            }
+            if (height[i] > highest){
+                highest = height[i];
+                posH = i + 1;
+            }
+            if (height[i] < lowest){
+                lowest = height[i];
+                posL = i + 1;
+            }
+        }
+
+        scanner.close();
+
+        System.out.println("O aluno mais alto e o " + posH + "°, com " + highest + " e o mais baixo e o " + posL + "°, com " + lowest);
+
+    }
+}
